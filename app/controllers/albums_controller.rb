980cdb522171269bb06f6587965e218e408d24dc
Duc <strawberrycsgo@gmail.com>
2018-08-10 09:10:10 +0000

    image_upload

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,63 @@
+class AlbumsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @albums = Album.all
+  end
+
+  def show
+    @images = @album.images.all
+  end
+
+  def new
+    @album = Album.new
+    @image = @album.images.build
+  end
+
+  def edit
+  end
+
+  def create
+    @album = Album.new(item_params)
+
+    respond_to do |format|
+      if @album.save
+        params[:images]['image_url'].each do |a|
+          @image = @album.images.create!(:image_url => a)
+        end
+        format.html { redirect_to @album, notice: 'Item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @album.update(item_params)
+        params[:images]['image_url'].each do |a|
+          @image = @album.images.create!(:image_url => a)
+        end
+        format.html { redirect_to @album, notice: 'Item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @album.destroy
+    respond_to do |format|
+      format.html { redirect_to albums_url, notice: 'Item was successfully destroyed.' }
+    end
+  end
+
+  private
+  def set_item
+    @album = Album.find(params[:id])
+  end
+
+  def item_params
+    params.require(:album).permit(:title, images_attributes: [:id, :album_id, :image_url])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -31,4 +31,18 @@ ActiveRecord::Schema.define(version: 20180810080316) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
+  create_table "albums", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "album_id"
+    t.string   "image_url"
+  end
+
 end
