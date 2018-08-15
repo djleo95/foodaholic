4af799b5b697e0d3dbcbe65476c0769662e7ba30
KieuThuong <kieuthuonghust@gmail.com>
2018-08-15 06:36:50 +0000

    seach

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @albums = Album.all
+    @search = User.search(param[:q])
+    @users = @search.result 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,13 @@ ActiveRecord::Schema.define(version: 20180810084757) do
     t.datetime "updated_at"
   end
 
+
   create_table "tags", force: :cascade do |t|
-    t.string "title"
+    t.string   "name"
+    t.string   "mail"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
