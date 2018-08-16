863496fa8d3730b8f542d501592212bba6604027
Kota Iizumi <al17010@shibaura-it.ac.jp>
2018-08-16 09:47:55 +0000

    edit CSS

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180815083327) do
+ActiveRecord::Schema.define(version: 20180816034719) do
 
   create_table "albums", force: :cascade do |t|
     t.integer  "user_id"
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 20180815083327) do
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "following_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "shares", force: :cascade do |t|
