dec2b4608342f0309e729c5ed296a1273a4a537a
井口　翔太 <hagetakatawasi@gmail.com>
2018-08-17 06:37:32 +0000

    css2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     def show
         @user = User.find_by_id(params[:id])
         @albums =  @user.albums
+        @shares = @user.shares
     end
 end
