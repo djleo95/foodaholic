8183d84cd9f95e7c6fd809650eb301674c33a48c
井口　翔太 <hagetakatawasi@gmail.com>
2018-08-17 06:37:32 +0000

    css2

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,4 +1,10 @@
 class Share < ActiveRecord::Base
     belongs_to :shareable, polymorphic: true
     belongs_to :user
+    
+    def get_shared_object
+        if(self.shareable_type == 'Image')
+            Image.find_by_id(self.shareable_id)
+        end
+    end
 end
